ce449d20dca8c968854afa642cd93a3b3a7b28f6
John McGettrick <jmcgettrick@iparadigms.com>
2016-01-20 10:53:23 +0000

    Bump version number.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$plugin->version = 2015040110;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2015040111;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = '2.3+';
+$plugin->release  = '2.6+';
 $plugin->dependencies = array('mod_turnitintooltwo' => 2015040110);
 $plugin->component = 'block_turnitin';
