709a8824b7dcb9cbedc0ec5756a626d4f648124a
Olivier SECRET <olivier.secret@catalyst-au.net>
2018-07-25 13:57:14 +1000

    Add privacy provider

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$plugin->version = 2016011105;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2018072500;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2012062500; // YYYYMMDDHH (This is the release version for Moodle 2.3)
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release  = '2.6+';
-$plugin->dependencies = array('mod_turnitintooltwo' => 2016011104);
+$plugin->dependencies = array('mod_turnitintooltwo' => 2018042701);
 $plugin->component = 'block_turnitin';
